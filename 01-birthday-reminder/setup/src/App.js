275914788fd6c1ead422979908767562129465ca
j15527 <mlastof@gmail.com>
2021-11-08 12:16:39 -0600

    work

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [value, setValue] =useState(data)
+  const displayValue = (index) =>{
+    return value[index].name
+  }
+  return<>
+    <button className={'btn'} onClick={() => setValue(1)}>Click me!</button>
+  </>
 }
+
+
 
 export default App;
